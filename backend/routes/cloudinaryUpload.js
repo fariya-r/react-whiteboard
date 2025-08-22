c68ed32da9c6be92b5a4ab9cc054280d8ae1fdc3
admin <mariyaakram123@gmail.com>
2025-08-22 13:41:37 +0500

    Updated cloudinary

--- backend/routes/cloudinaryUpload.js
+++ backend/routes/cloudinaryUpload.js
@@ -2,16 +2,15 @@
 const express = require("express");
 const multer = require("multer");
 const { v2: cloudinary } = require("cloudinary");
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const CloudinaryStorage = require("multer-storage-cloudinary").CloudinaryStorage;
 
 const router = express.Router();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
-  
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const cloudStorage = new CloudinaryStorage({
   cloudinary,
@@ -24,7 +23,7 @@ const cloudStorage = new CloudinaryStorage({
 const uploadCloud = multer({ storage: cloudStorage });
 
 router.post("/upload-file-cloud", uploadCloud.single("file"), (req, res) => {
-  res.json({ url: req.file.path });
+  return res.json({ url: req.file.path });
 });
 
 module.exports = router;
